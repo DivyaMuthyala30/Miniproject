82d1a8b0af667467b38ac86be012d98fc35fd539
divyamuthyala30 <divya.mutthyala@codexbox.com>
2022-06-13 20:31:58 +0530

    code changes

---
+++ src/Redux/Reducers/auth_reducer.js
@@ -0,0 +1,33 @@
+import {Auth_types } from "../Actiontypes/Auth_types";
+
+const initialState = {};
+
+export default function AuthReducer(state = initialState, action = null) {
+  if (!state) {
+    state = initialState;
+  }
+  switch (action.type) {
+    case Auth_types.LOGIN_SUCCESS: {
+      // const { token } = action.data;
+
+      localStorage.setItem("token", 123);
+      // localStorage.setItem("user", JSON.stringify(action.data));
+      return {
+        // ...action.data,
+      };
+    }
+    case Auth_types.LOGOUT_SUCCESS: {
+      localStorage.clear();
+      return {
+        
+      }
+    }
+    case Auth_types.POST_SUCCESS: {
+      return {
+        
+      }
+    }
+    default:
+      return state;
+  }
+}
